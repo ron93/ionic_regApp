023394ff7c74b08617ea7ec84c2161b5ba5166d1
ron93 <ronmkare1@gmail.com>
2017-10-12 08:44:45 +0300

    PongBall class->velocity declared-- move function-> ball movement and position

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ from kivy.uix.widget import Widget
 class PongGame(Widget):
    pass
 
+#Ball
+class PongBall(Widget):
+	#ball's velocity on the x and y axis
+	velocity_x = NumericProperty(0)
+	velocity_y = NumericProperty(0)
+
+	#referencelist property so as to use ball.velocity as a shorthand, like :- pos for w.x and w.y
+	velocity = ReferenceListProperty(velocity_x, velocity_y)
+	#`move` function to move ball one step.
+	#function will be called at equal intevals to animate ball
+	def move(self):
+		self.pos =vector(*Self.velocity) + self.pos 
+
+
 class PongApp(App):
     def build(Self):
         return PongGame()
