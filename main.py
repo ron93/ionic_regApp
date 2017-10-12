b0e672c6b8cd184a4993aae00f969991299abf5b
ron93 <ronmkare1@gmail.com>
2017-10-12 09:29:41 +0300

    PongGame class->serve_ball function

--- main.py
+++ main.py
@@ -4,19 +4,35 @@ from kivy.app import App
 # widget
 from kivy.uix.widget import Widget
 # properties
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty,ObjectProperty
 # vector
 from kivy.vector import Vector
-
+# clock
+from kivy.clock import Clock
+from random import randint
 class PongGame(Widget):
 	#hooking PongBall child widget(in pong.kv file) to PongGame class.
 	ball = ObjectProperty(None)
-   
-	def update(self, dt):
-   		#call ball.move and any other method
-   		pass
 
-#Ball
+	def serve_ball(self):
+		# ball position
+		self.ball.center= self.center
+		# ball velocity
+		self.ball.velocity = Vector(4, 0).rotate(randint(0,360))
+
+
+	def update(self, dt):
+   		self.ball.move()
+
+   		#bounce off top and bottom side of 'wall'
+   		if (self.ball.y < 0) or(self.ball.top > self.height):
+   				self.ball.velocity_y *= -1
+
+   		# bounce off left and righ side
+   		if (self.ball.x < 0 )  or (self.ball.right > self.width):
+   		self.ball.velocity_x *=-1 
+
+#PongBall class
 class PongBall(Widget):
 	#ball's velocity on the x and y axis
 	velocity_x = NumericProperty(0)
@@ -27,15 +43,15 @@ class PongBall(Widget):
 	#`move` function to move ball one step.
 	#function will be called at equal intevals to animate ball
 	def move(self):
-		self.pos =vector(*Self.velocity) + self.pos
+		self.pos =Vector(*self.velocity) + self.pos
 
 
 
 class PongApp(App):
     def build(Self):
     	game =PongGame()
-    	clock.schedule_interval(game.update, 1.0/60.0)
-		return game
+    	Clock.schedule_interval(game.update, 1.0/60.0)
+	return game
 
 if __name__ == '__main__':
 	PongApp().run()
