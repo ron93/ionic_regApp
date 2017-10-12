cc7d98a2ae3c000cdf1ea5cb368472bb4546ee11
ron93 <ronmkare1@gmail.com>
2017-10-12 07:53:44 +0300

    pong game start

---
+++ main.py
@@ -0,0 +1,14 @@
+import kivy
+kivy.require('1.10.0')
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class PongGame(Widget):
+   pass
+
+class PongApp(App):
+    def build(Self):
+        return PongGame()
+
+if __name__ == '__main__':
+    PongApp().run()
